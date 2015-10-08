5ef809658cae7927fe9ab745f21e033a747824eb
vfarcic <viktor@farcic.com>
2015-10-08 18:36:13 +0200

    Change to a fixed Ansible version

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing Ansible..."
-apt-get install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install -y ansible
+apt-get install -y python-pip python-dev
+pip install ansible==1.9.2
+mkdir -p /etc/ansible
+touch /etc/ansible/hosts
